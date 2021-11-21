1f730f1bd318e21df42290882464acfca14cae03
Amal Qasim <amal.qasim@ufst.dk>
2021-11-21 21:27:46 +0100

    controllers were added

---
+++ PlatformService/PlatformService/Data/PrepDb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using( var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding data...");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "YO", Publisher = "Me", Cost = "Not free" },
+                    new Platform() { Name = "Jay", Publisher = "Mart", Cost = "Ok free" },
+                    new Platform() { Name = "PC", Publisher = "Mac", Cost = "Maybe free" },
+                    new Platform() { Name = "Ice", Publisher = "Freeze", Cost = "Free" },
+                    new Platform() { Name = "Bank", Publisher = "DB", Cost = "All free" }
+                    );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+        }
+    }
+}
